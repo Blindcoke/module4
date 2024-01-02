5751ab8443f1f2f6a3b55b31d60522728c61c9db
sage <92391931+Blindcoke@users.noreply.github.com>
2024-01-02 22:13:53 +0900

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,16 @@
+
+
+
+( function speakHello (window){
+
+    var speakWord = "Hello";
+
+    var helloSpeaker = {};
+
+    helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+    }
+
+    window.helloSpeaker = helloSpeaker;
+
+})(window);
